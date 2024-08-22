61b554b2b6515a5d63d703ed0ceb06ddddfa3779
Jinny <asdfghjk1052@naver.com>
2024-08-23 01:31:04 +0900

    [ :sparkles: | FEATURE | 제로초 숫자야구 (아직 진행 중) ]

---
+++ 7. 숫자야구/quiz.js
@@ -0,0 +1,23 @@
+const answer = [];
+for (let n = 0; n < 4; n++) {
+  answer.push(n + 2);
+}
+console.log(answer);
+
+const answer2 = [];
+for (let n = 0; n <= 3; n++) {
+  answer2.push(n + 2);
+}
+console.log(answer2);
+
+const answer3 = [];
+for (let n = 1; n < 4; n++) {
+  answer3.push(n + 1);
+}
+console.log(answer3);
+
+const answer4 = [];
+for (let n = 1; n <= 3; n++) {
+  answer3.push(n + 1);
+}
+console.log(answer4);
